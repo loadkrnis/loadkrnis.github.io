0bc6c1127591878efcfff3bbd63bbb4ccc87911d
박민규 <charming@woowahan.com>
2022-10-16 15:37:06 +0900

    한글화 & workflow 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'loadkrnis', // Usually your GitHub org/user name.
+  projectName: 'loadkrnis.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -41,7 +41,6 @@ const config = {
         },
         blog: {
           showReadingTime: true,
-          archiveBasePath: "archive",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -76,7 +75,6 @@ const config = {
             label: 'GitHub',
             position: 'right',
           },
-          { to: "/archive", label: "Archive", position: "left" },
         ],
       },
       footer: {
